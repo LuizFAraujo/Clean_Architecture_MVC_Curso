8c00ea56008f84aaf259704cf80b8b8cd76be9fa
Luiz Fernando de Araujo <luizfaraujo.eng@gmail.com>
2022-11-30 00:07:00 -0300

    Criação Entidades.

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,52 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+
+        // Relacionamento
+        public ICollection<Product> Products { get; private set; }
+
+
+        //----------- Construtor
+        public Category(string name)
+        {
+            //Name = name;
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Id Inválido!");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Nome Inválido! Nome é requerido.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Nome Inválido! Muito curto, minímo 3 caracteres.");
+
+            Name = name;
+        }
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+
+        //public DateTime CreateDate { get; protected set; }
+        //public DateTime ModifiedDate { get; protected set; }
+        //public string CreatedBy { get; protected set; }
+        //public string ModifiedBy { get; protected set; }
+    }
+}
