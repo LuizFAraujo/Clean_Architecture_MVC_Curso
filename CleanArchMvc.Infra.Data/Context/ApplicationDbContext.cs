c363730332ec87212a6810297fa6b8cb48f990d0
Luiz Fernando de Araujo <luizfaraujo.eng@gmail.com>
2022-12-01 23:09:13 -0300

    Implementado Infra Data.

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Domain.Entities;
+//using CleanArchMvc.Infra.Data.EntitiesConfiguration;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Category> Categories { get; set; } // Mapeando Category para a tabela Categories
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // busca os arquivos, individuais, de configuração.
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
